f2cb6956a49602d1715dd5a89060460ebef4813e
medardo <metejada@bgeneral.com>
2022-09-20 11:27:12 -0500

    Variables

---
+++ HolaMundoJava/src/Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main(String[] args) {
+        int speed;
+        speed = 0;
+        System.out.println(speed);
+
+        int salary = 1000;
+        String employeeName = "Medardo";
+        System.out.println(employeeName);
+    }
+}
